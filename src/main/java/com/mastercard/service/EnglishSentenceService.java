b8d2071f154ccfb3be8aba804282c89be3e49fc1
Sravan Cheriyala <sravan.k.cheriyala@ey.com>
2018-07-27 09:55:05 -0400

    created new methods using String.split

--- src/main/java/com/mastercard/service/EnglishSentenceService.java
+++ src/main/java/com/mastercard/service/EnglishSentenceService.java
@@ -1,15 +1,13 @@
 package com.mastercard.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
 public class EnglishSentenceService implements SentenceService {
-            /*
 
-            My below algorithm has  Time complexity  O(N) and Space complexity O(1)
-
-
+    /*
 
         Assumptions :
                     1.When null or empty i assumed output to be null and empty string respective and length will be 0
@@ -18,7 +16,13 @@ public class EnglishSentenceService implements SentenceService {
                       However i have created new bonus method called findAllLongestWords() where it will give all the longest words.
                       I kept it as seprate method because time complexity varies for both methods
 
-                */
+   */
+        /*
+             below algorithm has  Time complexity  O(N) and Space complexity O(1)
+
+            I havent used string.split ot StringTokenizer in this method becasue doing so will increase the Time and space complexity
+           .I wrote a method called findLongestWord2 where it uses string.split please see scroll to see
+         */
         public LongestWordResult findLongestWord(String sentence) {
 
                 LongestWordResult result = null;
@@ -49,7 +53,6 @@ public class EnglishSentenceService implements SentenceService {
                 result= new LongestWordResult(word);
                 return result;
         }
-
 
 
     /**
@@ -106,6 +109,59 @@ public class EnglishSentenceService implements SentenceService {
             }
             return results;
         }
+
+
+        /*
+        *
+        * Below code is using String.split fiuntion . We have concise code but time and space complexity increased
+        *  Time complexity : O(n+K)
+         * Space complexity: O(n)
+         *
+        * */
+        public  LongestWordResult  findLongestWord2(String sentence){
+            if(sentence == null || sentence.length()==0) {
+                return new LongestWordResult(sentence);
+            }
+            sentence = removePunctuations(sentence);
+            String[] words = sentence.split(" ");//time:O(n) space:O(n)
+            LongestWordResult  result = null;
+            int longestWordLength =Integer.MIN_VALUE;
+            for(String word:words){  //O(k)
+                if(longestWordLength < word.length())  {
+                    result = new LongestWordResult(word);
+                    longestWordLength=word.length();
+                }
+            }
+            return  result;
+        }
+
+       /*this is using java String.split() method. We have concise code but the time and space complexity increased
+       *
+       * Time complexity : O(n+K)
+       * Space complexity: O(n)
+       * */
+         public LongestWordResult[] findAllLongestWords2(String sentence){
+             if(sentence == null || sentence.length()==0) {
+                 LongestWordResult[] results  = new LongestWordResult[1];
+                 results[0] = new LongestWordResult(sentence);
+                 return  results;
+             }
+             //removing all the punctuation marks
+             sentence = removePunctuations(sentence);
+             String[] words = sentence.split(" ");//time:O(n) space:O(n)
+             List<LongestWordResult> longestWords = new ArrayList<LongestWordResult>();
+             int longestWordLength =Integer.MIN_VALUE;
+             for(String word:words){  //O(k)
+                  if(longestWordLength < word.length())  {
+                      longestWords.clear();
+                      longestWords.add(new LongestWordResult(word));
+                      longestWordLength=word.length();
+                  }else if(longestWordLength == word.length()){
+                      longestWords.add(new LongestWordResult(word));
+                  }
+             }
+             return  longestWords.toArray(new LongestWordResult[0]);
+         }
 
 
         private String removePunctuations(String sentence) {
--- src/main/java/com/mastercard/service/SentenceService.java
+++ src/main/java/com/mastercard/service/SentenceService.java
@@ -2,5 +2,6 @@ package com.mastercard.service;
 
 public interface SentenceService {
     public LongestWordResult findLongestWord(String sentence);
+
     public LongestWordResult[] findAllLongestWords(String sentence);
 }
